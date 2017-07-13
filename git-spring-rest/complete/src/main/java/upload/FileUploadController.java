7e111351f19105b4ee789a614e26db59530ccc8a
Vinod Kelbaikar <vinod.kelbaikar@gmail.com>
2017-07-12 20:45:19 -0400

    Test

---
+++ git-spring-rest/complete/src/main/java/upload/FileUploadController.java
@@ -0,0 +1,65 @@
+package upload;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/upload-file")
+public class FileUploadController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping(method=RequestMethod.GET)
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response uploadFile(
+            @FormDataParam("file") InputStream uploadedInputStream,
+            @FormDataParam("file") FormDataContentDisposition fileDetail) {
+        // check if all form parameters are provided
+        if (uploadedInputStream == null || fileDetail == null)
+            return Response.status(400).entity("Invalid form data").build();
+        // create our destination folder, if it not exists
+        try {
+            createFolderIfNotExists(UPLOAD_FOLDER);
+        } catch (SecurityException se) {
+            return Response.status(500)
+                    .entity("Can not create destination folder on server")
+                    .build();
+        }
+        String uploadedFileLocation = UPLOAD_FOLDER + fileDetail.getFileName();
+        try {
+            saveToFile(uploadedInputStream, uploadedFileLocation);
+        } catch (IOException e) {
+            return Response.status(500).entity("Can not save file").build();
+        }
+        return Response.status(200)
+                .entity("File saved to " + uploadedFileLocation).build();
+    }
+
+    private void saveToFile(InputStream inStream, String target)
+            throws IOException {
+        OutputStream out = null;
+        int read = 0;
+        byte[] bytes = new byte[1024];
+        out = new FileOutputStream(new File(target));
+        while ((read = inStream.read(bytes)) != -1) {
+            out.write(bytes, 0, read);
+        }
+        out.flush();
+        out.close();
+    }
+
+    private void createFolderIfNotExists(String dirName)
+            throws SecurityException {
+        File theDir = new File(dirName);
+        if (!theDir.exists()) {
+            theDir.mkdir();
+        }
+    }
+
+}
---
+++ git-spring-rest/initial/src/main/java/upload/FileUploadConfiguration.java
@@ -0,0 +1,13 @@
+package upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploadConfiguration {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileUploadConfiguration.class, args);
+    }
+
+} 
